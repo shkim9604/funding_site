2de5026df98b1c71159a282c73e64a26c25953c2
shkim9604 <shkim9604@naver.com>
2023-02-28 17:39:31 +0900

    로그인 및 회원가입 기능

--- user/views.py
+++ user/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import check_password, make_password
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
 from django.views import View
-
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .models import User as f_User
 
 # Create your views here.
 
@@ -12,24 +13,41 @@ def index(request):
 
 
 class LoginView(View):
+    template_name = 'login.html'
     def get(self, request):
         return render(request, 'login.html')
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         email = request.POST.get('email')
         password = request.POST.get('password')
         try:
-            user = User.objects.get(email=email)
-        except User.DoesNotExist:
+            user = f_User.objects.get(email=email)
+            if check_password(password, user.password):
+                request.session['user'] = user.id
+                return redirect("/")
+            else:
+                return render(request, 'login.html', {'error': '비밀번호가 일치하지않습니다.'})
+        except f_User.DoesNotExist:
             return render(request, 'login.html', {'error': '이메일이 존재하지 않습니다.'})
-        user = authenticate(request, user_mail=user.email, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-        else:
-            return render(request, 'login.html', {'error': '이메일 또는 비밀번호가 올바르지 않습니다.'})
+
 
 
 class RegisterView(View):
     def get(self, request):
         return render(request, 'register.html')
+
+    def post(self, request):
+        name = request.POST.get('name')
+        email = request.POST.get('Email')
+        password = request.POST.get('password')
+        re_password = request.POST.get('re-password')
+
+        if password != re_password:
+            return render(request, 'register.html', {'error': '비밀번호와 비밀번호 확인이 일치하지않습니다.'})
+        else:
+            user = f_User.objects.create(
+                name=name,
+                email=email,
+                password=make_password(password)
+            )
+            return redirect("/login")
